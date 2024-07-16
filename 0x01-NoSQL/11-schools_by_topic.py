83f319c73f82efc7125f2eaf233800e4d3865a1d
YousefShata <yossef_shata_a@yahoo.com>
2024-07-16 09:09:54 -0700

    find all doc

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+find document model
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    find document based on topic
+    """
+    return [doc for doc in mongo_collection.find_many({"topic": topic})]
