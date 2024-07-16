f26bae066bbe9e94d2f3a247c6c107ea01a12b0c
YousefShata <yossef_shata_a@yahoo.com>
2024-07-16 08:52:02 -0700

    insert doc

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -8,6 +8,6 @@ import pymongo
 def insert_school(mongo_collection, **kwargs):
     """
     isert document
-    """ 
-    new_doc = mongo_collection.insert(kwargs)
+    """
+    new_doc = mongo_collection.insert_one(kwargs)
     return new_doc.inserted_id
