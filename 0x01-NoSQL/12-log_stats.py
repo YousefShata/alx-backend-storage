1e2c5b2eecd144d94d88fde293f08ac1ec1f6c94
YousefShata <yossef_shata_a@yahoo.com>
2024-07-16 11:01:29 -0700

    nginx status

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,25 +4,30 @@ nginx stats model
 """
 from pymongo import MongoClient
 
-client = MongoClient('localhost',27017)
-nginx_collection = client.logs.nginx
 
-log_count = nginx_collection.count_documents({})
+def log_stats(nginx_collection):
+    """
+    get nginx data
+    """
+    get_count = nginx_collection.count_documents({"method": "GET"})
+    post_count = nginx_collection.count_documents({"method": "POST"})
+    put_count = nginx_collection.count_documents({"method": "PUT"})
+    patch_count = nginx_collection.count_documents({"method": "PATCH"})
+    delete_count = nginx_collection.count_documents({"method": "DELETE"})
 
-get_count = nginx_collection.count_documents({"method": "GET"})
-post_count = nginx_collection.count_documents({"method": "POST"})
-put_count = nginx_collection.count_documents({"method": "PUT"})
-patch_count = nginx_collection.count_documents({"method": "PATCH"})
-delete_count = nginx_collection.count_documents({"method": "DELETE"})
+    path = nginx_collection.count_documents({"method": "GET", "path": "/status"})
 
-path = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+    print("{} logs".format(log_count))
+    print("Methods:")
+    print("\tmethod GET: {}".format(get_count))
+    print("\tmethod POST: {}".format(post_count))
+    print("\tmethod PUT: {}".format(put_count))
+    print("\tmethod PATCH: {}".format(patch_count))
+    print("\tmethod DELETE: {}".format(delete_count))
+    print("{} status check".format(path))
 
-print("{} logs".format(log_count))
-print("Methods:")
-print("\tmethod GET: {}".format(get_count))
-print("\tmethod POST: {}".format(post_count))
-print("\tmethod PUT: {}".format(put_count))
-print("\tmethod PATCH: {}".format(patch_count))
-print("\tmethod DELETE: {}".format(delete_count))
-print("{} status check".format(path))
+if __name__ == "__main__":
+    client = MongoClient('localhost',27017)
+    nginx_collection = client.logs.nginx
 
+    log_stats(nginx_collection)
