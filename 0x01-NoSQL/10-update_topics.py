33a0bae00f316d0ca0f84a1b19298a6f52e5d971
YousefShata <yossef_shata_a@yahoo.com>
2024-07-16 09:03:01 -0700

    update doc

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+insert document model
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    isert document
+    """
+    mongo_collection.update({"name": name}, {"$push": {"topics": topics}})
