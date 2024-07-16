975dc0526a56836ee3a5a5ae67c826edb29e74ff
YousefShata <yossef_shata_a@yahoo.com>
2024-07-16 09:05:55 -0700

    set all doc

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -8,4 +8,4 @@ def update_topics(mongo_collection, name, topics):
     """
     isert document
     """
-    mongo_collection.update({"name": name}, {"$push": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
