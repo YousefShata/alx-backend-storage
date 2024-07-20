226b639e850e5f30fb927cef9d033f49a484f60d
YousefShata <yossef_shata_a@yahoo.com>
2024-07-19 18:00:06 -0700

    setup cache

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+redis
+"""
+import redis
+from uuid import uuid4
+
+
+class Cache:
+    """
+    cashe class
+    """
+    
+    def __init__(self):
+        """
+        Initialize the cache.
+        """
+        self._redis = redis.Redis()
+        self._redis.flushdb
+
+    def store(self, data) -> str:
+        """
+        return key after storing input
+        """
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
